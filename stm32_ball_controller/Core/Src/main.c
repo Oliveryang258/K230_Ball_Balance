65c82e0431b30e50f4d81e747442c1a87cf6acd1
Oliveryang258 <3214224351@qq.com>
2026-07-30 00:11:52 +0800

    Remove PA13 LED GPIO code

--- stm32_ball_controller/Core/Src/main.c
+++ stm32_ball_controller/Core/Src/main.c
@@ -548,7 +548,6 @@ int main(void)
 	g_imu_status = (uint8_t)ICM20602_GetStatus();
 	g_imu_who_am_i = ICM20602_GetWhoAmI();
 	g_imu_address = ICM20602_GetAddress();
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13, GPIO_PIN_SET);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -726,14 +725,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
-  {
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_13;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-  }
+
   /* USER CODE END MX_GPIO_Init_2 */
 }
 
